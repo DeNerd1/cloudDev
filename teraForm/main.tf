10764e2aa3d6a961bc3ce56b7ea57c5ef111ed22
tanyi <tanyif25@gmail.com>
2026-02-11 16:37:06 +0100

    Teraform local

---
+++ teraForm/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    local ={
+        source = "hashicorp/local"
+        version = ">=2.0"
+    }
+  }
+}
+
+resource "local_file" "hello" {
+    content = "Hello world"
+    filename = "hello.txt"
+  
+}
