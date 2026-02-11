c0bcea07a45b2540554ae4da310491bb1b688bae
tanyi <tanyif25@gmail.com>
2026-02-11 16:50:02 +0100

    aws s3bk

--- teraForm/main.tf
+++ teraForm/main.tf
@@ -1,10 +1,26 @@
 terraform {
   required_providers {
+   aws ={
+    source = "hashicorp/aws"
+    version = ">=5.0"
+   }
+
+
     local ={
         source = "hashicorp/local"
         version = ">=2.0"
+
     }
   }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+
+resource "aws_s3_bucket" "demo_s3bk" {
+   bucket = "demo1-web-s3-bucket"
 }
 
 resource "local_file" "hello" {
